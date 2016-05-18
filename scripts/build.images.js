e60776896a6a6d77590b3438e9f3083eddad6f13
Jake Mulley <mulley.jake@me.com>
2016-05-18 12:17:25 +0100

    fix(package): remove imagemin

--- scripts/build.images.js
+++
@@ -1,7 +0,0 @@
-var argv     = require('minimist')(process.argv.slice(2));
-var Imagemin = require('imagemin');
-
-new Imagemin()
-  .src(argv.directory + '/**/*.{gif,jpg,png,svg}')
-  .dest(argv.output)
-  .run();
