88e664cad8a28132e952383549aa8b067aac70e8
Jake Mulley <mulley.jake@me.com>
2016-06-06 15:27:49 +0100

    fix(watch): remove semicolon

--- scripts/watch.js
+++ scripts/watch.js
@@ -17,7 +17,7 @@ gaze(argv._, function(err, watcher) {
       exec(task, function(error, stdout, stderr) {
         if(error === null) {
           console.log(stderr);
-          console.log('Task "' + task + '" run successfully:');
+          console.log('Task "' + task + '" run successfully');
           if(stdout) {
             console.log(stdout);
           }
